419abe3c3ee151831ab3bfe15c4b1e12a48af060
José Lugo <chepe.time@gmail.com>
2020-05-13 18:50:33 -0500

    Added GitHub action for GHPages deploy v12

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React, plz
+          Learn React, update plz
         </a>
       </header>
     </div>
